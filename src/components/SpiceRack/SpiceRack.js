40e2c6cd2ccf983e4381b4a82ec2bb24328c4438
Mike Fallesen <mfallesen2010@gmail.com>
2021-01-27 16:45:19 -0800

    add initial Files

---
+++ src/components/SpiceRack/SpiceRack.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SpiceRack() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/SpiceRack/index.js
@@ -0,0 +1,3 @@
+import SpiceRack from "./SpiceRack"
+
+export default SpiceRack;
