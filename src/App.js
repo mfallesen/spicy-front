cebcd23329b4bb70ab9407b4ef1d12a9246ed769
Mike Fallesen <mfallesen2010@gmail.com>
2021-01-28 12:31:25 -0800

    added basic footer placemenr for later integration

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <LandingPage/>
+      <Footer></Footer>
       
     </div>
   );
