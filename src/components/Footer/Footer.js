75012da1ee80eea393e56c48fd7e6ee7e52b4b1e
Mike Fallesen <mfallesen2010@gmail.com>
2021-01-28 12:33:01 -0800

    update and add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Pane, Text } from 'evergreen-ui'
+
+export default function Footer() {
+    return (
+        <Pane
+        height={`200px`}
+        >
+            <Text>Some lovely text here</Text>
+        </Pane>
+        
+    )
+}
