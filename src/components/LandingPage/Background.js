80460364ddb4bd68a0bda80e0d8d26368e1be355
Mike Fallesen <mfallesen2010@gmail.com>
2021-01-27 16:42:36 -0800

    add Landing page files and inital layout to repo

---
+++ src/components/LandingPage/Background.js
@@ -0,0 +1,15 @@
+import { Pane } from 'evergreen-ui'
+import React from 'react'
+
+
+
+export default function Background() {
+    return (
+        <Pane
+        height={500}
+        backgroundColor='transparent'
+        backgroundImage={'url(https://res.cloudinary.com/dyn9tmf2i/image/upload/v1611791392/SpiceAppBackgroundMed_gcmiqr.png)'}
+        >
+        </Pane>
+    )
+}
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,37 @@
+import { Heading, Pane } from 'evergreen-ui'
+import React from 'react'
+import Background from './Background'
+import './Landing.css'
+
+
+export default function LandingPage() {
+    return (
+        
+
+           
+
+                <Pane
+                    display="flex"
+                    height={`600px`}
+                    width={'100%'}
+                    justifyContent="center"
+                    alignItems='center'
+                    backgroundImage={'url(https://res.cloudinary.com/dyn9tmf2i/image/upload/v1611791392/SpiceAppBackgroundMed_gcmiqr.png)'}
+                >
+                    <Pane
+                    width={`60%`}
+                    className={'blurredBG'}
+                    borderRadius={`10px`}
+                    paddingBottom='40px'
+                    paddingLeft='40px'
+                    paddingRight='40px'
+                    >
+
+                    <Heading color='white' size={800} marginTop='default'>Welcome to Your Virtual Spice Rack</Heading>
+                    <Heading color='white' size={600} marginTop='default'>We help you keep your spice rack organized, virtually speaking of course, we can't control how you organize them at home.</Heading>
+                    </Pane>
+                </Pane>
+           
+       
+    )
+}
