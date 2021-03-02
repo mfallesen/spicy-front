75f4c3c64be0db647e224bedf3e292233ed2f1ed
Mike Fallesen <mfallesen2010@gmail.com>
2021-03-01 17:39:53 -0800

    add basic components for sign in form

---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,50 @@
+import { Container, Grid, makeStyles, TextField, Typography } from '@material-ui/core'
+import React from 'react'
+
+const useStyles = makeStyles((theme) => ({
+    inputbox: {
+        marginLeft: theme.spacing(3),
+        marginRight: theme.spacing(3)
+    }
+}))
+
+export default function SignIn() {
+
+    const classes = useStyles()
+    return (
+
+        <Container>
+            <Grid container justify='center' direction='column' alignItems='center'>
+                <Typography component='h2'> Ready to take control of your Spicy Life?</Typography>
+                {/* NoValidate only for development and testing */}
+                <from noValidate>
+                    <Grid container spacing={3} justify='center' direction='column'>
+
+                        <Grid item spacing={3}>
+                        <TextField
+                            className={classes.inputbox}
+                                required
+                                variant='outlined'
+                                name='name'
+                                label='Desired User Name:'
+                                placeholder='Witty Username Here:'
+                            />
+                            <TextField
+                            className={classes.inputbox}
+                                required
+                                variant='outlined'
+                                name='password'
+                                label='Password:'
+                                placeholder='Enter Password:'
+                            />
+                            
+
+                        </Grid>
+                    </Grid>
+                </from>
+            </Grid>
+
+        </Container>
+
+    )
+}
