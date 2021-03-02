df6121096eb39c519eee0c79164760e36525f4cf
Mike Fallesen <mfallesen2010@gmail.com>
2021-03-01 17:39:22 -0800

    Added Nav Menu and some basic animation to menu on scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,62 @@
+import {Button, makeStyles, Menu, MenuItem} from '@material-ui/core'
+import {React, useEffect, useState} from 'react'
 
-import React from 'react'
+
 
 
 export default function Navbar() {
-    return (
-       <div>
 
-       </div>
+    const [scrollState, setScrollState] = useState('top')
+    
+    useEffect(() => {
+        let navScroll = null
+        navScroll = document.addEventListener('scroll', scrollEvent => {
+            let scrolled = document.scrollingElement.scrollTop
+            if (scrolled >= 100) {
+                setScrollState('notTop')
+            } else {
+                setScrollState('top')  
+            }
+        })
+        return () => {
+            document.removeEventListener('scroll', navScroll)
+        }
+    }, [scrollState])
+
+    const [anchorEl, setAnchorEl]=useState(null)
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+    
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
+
+    return (
+        <div style={{
+            backgroundColor: scrollState === 'top' ? 'transparent' : 'grey',
+            color: scrollState === 'top' ? 'white' : 'black',
+            transitionDuration: '1s',
+            position: 'fixed',
+            marginTop: '5px',
+            marginLeft:'5px',
+            borderRadius: '5px'
+        }}>
+        <Button aria-controls="simple-menu" color='inherit' aria-haspopup="true" onClick={handleClick}>
+          Open Menu
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>My Spice Rack</MenuItem>
+          <MenuItem onClick={handleClose}>My Account</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </div>
     )
 }
