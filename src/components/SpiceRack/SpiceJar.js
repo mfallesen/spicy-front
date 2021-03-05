0a98f9473252a7de1eab22db472ca16d10ee8969
Mike Fallesen <mfallesen2010@gmail.com>
2021-03-04 16:52:27 -0800

    utilize props to render individual spices

--- src/components/SpiceRack/SpiceJar.js
+++ src/components/SpiceRack/SpiceJar.js
@@ -1,13 +1,17 @@
 import { Grid, Avatar, Typography } from '@material-ui/core'
 import React from 'react'
 
-export default function SpiceJar() {
+export default function SpiceJar(props) {
+
+    console.log(props);
+
     return (
         <Grid item>
-            <Avatar>C</Avatar>
-            <Typography>Name of Spice</Typography>
-            <Typography>purchase Date</Typography>
-            <Typography>Best By Date</Typography>
+            <Avatar>{props.name.slice(0,1)}</Avatar>
+            <Typography>{props.name}</Typography>
+            <Typography>{props.brand}</Typography>
+            <Typography>{props.purchaseDate}</Typography>
+            <Typography>{props.expires}</Typography>
         </Grid>
     )
 }
