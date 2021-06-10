d106c24b49349e86ed7356a471ae9881a1b6af1e
Mike Fallesen <mfallesen2010@gmail.com>
2021-06-09 18:38:09 -0700

    Basic styling and functionality added to the buttons in the bottom. May add more as time progresses

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,32 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Link, Typography } from '@material-ui/core'
 import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
+import './Footer.css'
 
 export default function Footer() {
     return (
-        <div>
-            <Grid container justify='center' spacing={6}>
-                <Grid item>
-                    <Typography>Copyright 2021 Mike Fallesen</Typography>
-                </Grid>
-                <Grid item>
-                    <Typography></Typography>
-                </Grid>
-                <Grid item >
-                    <Typography>Need to get in touch?</Typography>
-                    <Typography><GitHubIcon/>Found an Issue? Open an issue on GitHub</Typography>
-                    <Typography><EmailIcon/>You can also reach me through Email</Typography>
-                    
-                    
-                </Grid>
+        <Grid container justify='center' spacing={6} className='footer-Main'>
+            <Grid item >
+                <Typography>Copyright 2021 Mike Fallesen</Typography>
+            </Grid>
+            <Grid item >
+                <Typography>Need to get in touch?</Typography>
+
+                <Typography className='link-decoration' >
+                    <Link href='https://github.com/mfallesen/spicy-front/issues'>
+                        <GitHubIcon />  Found an Issue? Open an issue on GitHub
+                        </Link>
+                </Typography>
+                <Typography className='link-decoration' >
+                    <Link href='mailto:mfallesen2010@gmail.com'>
+                        <EmailIcon />  You can also reach me through Email
+                        </Link>
+                </Typography>
+
 
             </Grid>
-        </div>
+
+        </Grid>
     )
 }
